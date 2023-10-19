791bca9bdf1b0d39542abe1d1da9ce15da798808
0baldkiller0 <zhao2622805406@outlook.com>
2023-10-19 22:33:21 +0800

    modify for loop

--- BusAllocator.py
+++ BusAllocator.py
@@ -1,6 +1,16 @@
 from GridParameters import GridParameters
+import argparse
 
 
+
+class Bus:
+    def __init__(self, BusID, Bus_start, StartPads, Bus_end, EndPads, BusWidth):
+        self.BusID = BusID
+        self.Bus_start = Bus_start
+        self.StartPads = StartPads
+        self.Bus_end = Bus_end
+        self.EndPads = EndPads
+        self.BusWidth = BusWidth
 
 class BusAllocator:
     def __init__(self, clearance, trackwidth, grid_parameter: GridParameters):
@@ -9,11 +19,15 @@ class BusAllocator:
         self.clearance = clearance
         self.trackwidth = trackwidth
         self.BusList = []
+
+        
     def is_near(self,pad1, pad2, MaxDistance):
         dx = pad1.position.X - pad2.position.X
         dy = pad1.position.Y - pad2.position.Y
         distance = (dx**2+dy**2)**0.5
         return distance <= MaxDistance
+    
+
     def GenerateMultipadFPList(self):
         MultipadFPList = []
         for i in range(len(self.FootprintList)):
@@ -21,17 +35,13 @@ class BusAllocator:
                 MultipadFPList.append(self.FootprintList[i])
         return MultipadFPList
 
-
-#    def FindInNet(self, pad):
-#        for i in range(len(self.Netlist)):
-#            if 
     
     def NetsInFP(self, footprint):
         PadAndNet = []
         for pad in footprint.pads:
-            PadAndNet.append((pad, pad.net.number))
+            if pad.net is not None:
+                PadAndNet.append((pad, pad.net.number))
         return PadAndNet
-
 
 
 
@@ -48,7 +58,7 @@ class BusAllocator:
 #        BusWidth = 0
         BusID = 0
 
-        for i in range(len(MFPList)-1):
+        for i in range(len(MFPList)):
             PadNet1 = self.NetsInFP(MFPList[i])
             for j in range(len(MFPList)-1-i):
                 StartBusPins_temp = []
@@ -56,7 +66,7 @@ class BusAllocator:
                 BusWidth_temp = 0
                 count = 0
 
-                PadNet2 = self.NetsInFP(MFPList[j])
+                PadNet2 = self.NetsInFP(MFPList[i+j+1])
 #                for pad in MFPList[i].pads:
 #                    if pad.net in
 
@@ -69,29 +79,56 @@ class BusAllocator:
                             EndBusPins_temp.append(PadNet2[n][0])
                             BusWidth_temp += self.clearance + self.trackwidth
                             count +=1
-            if count >= 2:
-                start_sum_x = 0
-                start_sum_y = 0
-                end_sum_x = 0
-                end_sum_y = 0
-                for pin in StartBusPins_temp:
-                    start_sum_x += pin.position.X
-                    start_sum_y += pin.position.Y
+                if count >= 2:
+                    start_sum_x = 0
+                    start_sum_y = 0
+                    end_sum_x = 0
+                    end_sum_y = 0
+                    for pin in StartBusPins_temp:
+                        start_sum_x += pin.position.X
+                        start_sum_y += pin.position.Y
 
-                Bus_start_x = start_sum_x/len(StartBusPins_temp)
-                Bus_start_y = start_sum_y/len(StartBusPins_temp)
-                for pin in EndBusPins_temp:
+                    Bus_start_x = start_sum_x/len(StartBusPins_temp)
+                    Bus_start_y = start_sum_y/len(StartBusPins_temp)
+                    for pin in EndBusPins_temp:
 
-                    end_sum_x += pin.position.X
-                    end_sum_y += pin.position.Y
+                        end_sum_x += pin.position.X
+                        end_sum_y += pin.position.Y
 
-                Bus_end_x = end_sum_x/len(EndBusPins_temp)
-                Bus_end_y = end_sum_y/len(EndBusPins_temp) 
-                Bus_start = (Bus_start_x,Bus_start_y)
-                Bus_end = (Bus_end_x, Bus_end_y)
-                BusWidth = BusWidth_temp 
-                self.BusList.append((BusID, Bus_start, Bus_end, BusWidth))
-                BusID +=1
+                    Bus_end_x = end_sum_x/len(EndBusPins_temp)
+                    Bus_end_y = end_sum_y/len(EndBusPins_temp) 
+                    Bus_start = (Bus_start_x,Bus_start_y)
+                    Bus_end = (Bus_end_x, Bus_end_y)
+                    BusWidth = BusWidth_temp
+                    bus = Bus(BusID,Bus_start,StartBusPins_temp,Bus_end,EndBusPins_temp,BusWidth)
+                    self.BusList.append(bus)
+                    BusID +=1
+
+def allocator_arguments():
+    parser = argparse.ArgumentParser('BusAllocator')
+    parser.add_argument('--kicad_pcb', type=str, dest='kicad_pcb', default="bench1/bm1.unrouted.kicad_pcb")
+    parser.add_argument('--kicad_pro', type=str, dest='kicad_pro', default="bench1/bm1.unrouted.kicad_pro")
+    parser.add_argument('--save_file', type=str, dest='save_file', default="bench1/bm1.routed.kicad_pcb")
+    return parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    clearance = 100 
+    trackwidth = 23
+    arg = allocator_arguments()
+    benchmark_file = arg.kicad_pcb
+    project_file = arg.kicad_pro
+    save_file = arg.save_file
+    gridParameters = GridParameters(benchmark_file, project_file, save_file)
+    busallocator = BusAllocator(clearance, trackwidth, gridParameters)
+    busallocator.allocate()
+    for Bus in busallocator.BusList:
+        print(Bus.BusID,Bus.Bus_start,Bus.Bus_end,Bus.BusWidth)
+
+
+
+
 
                     
                 
